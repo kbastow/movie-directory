320bd8172bd7d2eab7b66fac7a948217202effb2
Kate Bastow <kate.b@montu.com.au>
2025-07-22 21:43:59 +1000

    Renaming pages folder to correct file pattern error.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import FavouritesPage from "../pages/FavouritesPage";
-import MoviePage from "../pages/MoviePage";
+import HomePage from "../page/HomePage";
+import FavouritesPage from "../page/FavouritesPage";
+import MoviePage from "../page/MoviePage";
 import AppLayout from "../layout/AppLayout";
-import PageNotFound from "../pages/PageNotFound";
+import PageNotFound from "../page/PageNotFound";
 
 const AppRoutes: React.FC = () => (
   <Routes>
