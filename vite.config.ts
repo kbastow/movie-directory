5467cbe784d00f93117ecc602b0f390c5f3392a9
Kate Bastow <kate.b@montu.com.au>
2025-07-22 17:44:12 +1000

    update to remove the base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/movie-directory/" : "/",
+export default defineConfig({
+  base: "/movie-directory/",
   plugins: [react()],
-}));
+});
