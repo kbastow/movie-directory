c837b8c3a0930d03be26301234d8a845b3105867
Kate Bastow <kate.b@montu.com.au>
2025-07-22 21:08:24 +1000

    Removing font imports

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,6 @@ import App from "./App.tsx";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./styles/theme.ts";
 import "./index.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
