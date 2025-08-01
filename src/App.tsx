0abeb72580b800e013b67553fe8e6856d43a6a22
Kate Bastow <kate.b@montu.com.au>
2025-08-01 12:59:35 +1000

    Adding public 404 page and RedirectHandler to fix error when refreshing a movieID page in production

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppRoutes from "./routes/AppRoutes";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useNavigate } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
+
+const RedirectHandler: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirectPath = localStorage.getItem("redirectPath");
+    if (redirectPath) {
+      localStorage.removeItem("redirectPath");
+      navigate(redirectPath, { replace: true });
+    }
+  }, [navigate]);
+  return null;
+};
 
 const App: React.FC = () => (
   <>
     <BrowserRouter basename="/movie-directory">
       <ScrollToTop />
       <QueryClientProvider client={queryClient}>
+        <RedirectHandler />
         <AppRoutes />
         <ReactQueryDevtools />
       </QueryClientProvider>
